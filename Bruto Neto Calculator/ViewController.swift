cb276273814fa8942a3d21f37ad31b4451f28cd5
Dimitar Stefanovski macMini <dimebt_2005@hotmail.com>
2017-12-20 11:35:17 +0100

    Initial Commit

---
+++ Bruto Neto Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bruto Neto Calculator
+//
+//  Created by Dimitar Stefanovski on 12/20/17.
+//  Copyright © 2017 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
